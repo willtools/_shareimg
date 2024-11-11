51744ba6394ca942bea5b9df844b59e822a7a0f1
willtools <88006096+willtools@users.noreply.github.com>
2024-11-11 22:22:18 +0700

    Update genimg.js

--- genimg.js
+++ genimg.js
@@ -1,32 +1,31 @@
-function gen() {
-  var _gambar1 = ["https://i.postimg.cc/jS4TSjRv/Screenshot-1.jpg", "https://i.postimg.cc/43PSr7Xk/Screenshot-10.jpg", "https://i.postimg.cc/NM0zYs0y/Screenshot-11.jpg", "https://i.postimg.cc/gJBB2Cwg/Screenshot-12.jpg", "https://i.postimg.cc/VvrZrKnm/Screenshot-13.jpg", "https://i.postimg.cc/Kj6qntDL/Screenshot-14.jpg", "https://i.postimg.cc/sfM34yJD/Screenshot-15.jpg", "https://i.postimg.cc/J7SMTgT3/Screenshot-16.jpg", "https://i.postimg.cc/FscmwZqM/Screenshot-17.jpg", "https://i.postimg.cc/wjpHzNY7/Screenshot-18.jpg"];
-  var _gambar2 = ["https://i.postimg.cc/TP26LXwx/Screenshot-2.jpg", "https://i.postimg.cc/x8chGfDr/Screenshot-3.jpg", "https://i.postimg.cc/J0vgPfMs/Screenshot-4.jpg", "https://i.postimg.cc/44Srf9VX/Screenshot-5.jpg", "https://i.postimg.cc/4xCqtRWM/Screenshot-6.jpg", "https://i.postimg.cc/7YJjfZn0/Screenshot-7.jpg", "https://i.postimg.cc/mgQqypqr/Screenshot-8.jpg", "https://i.postimg.cc/43s0LbkQ/Screenshot-9.jpg", "https://i.postimg.cc/Kv9rSPM5/1-AFDGVDFRHTDGF.jpg", "https://i.postimg.cc/13f09801/1-BFTGYHTFY57.jpg"];
-  var _gambar3 = ["https://i.postimg.cc/4xq6nXw7/1DRTRE4T.jpg", "https://i.postimg.cc/0NSpwhHt/2-ADRFTG5656.jpg", "https://i.postimg.cc/4dTpbwDX/2-BFGTHUTY6-RU6.jpg", "https://i.postimg.cc/0QdDtDvD/2-DFTGYHUJIYGI.jpg", "https://i.postimg.cc/90mTs0XS/2-DTGFYHRTYH.jpg", "https://i.postimg.cc/cCFYWWrJ/2-TFGYH5675-E.jpg", "https://i.postimg.cc/3r12mXnH/3-AFTGYHFJUYF5.jpg", "https://i.postimg.cc/RV0KRWDJ/3-AFTGYHT56-Y7.jpg", "https://i.postimg.cc/8PhQZCfD/3-BRFTGYHU5-RE7.jpg", "https://i.postimg.cc/W1TRCP0Z/3-GTHBTGFDY.jpg"];
-  var _gambar4 = ["https://i.postimg.cc/52gcMkmR/3-RFDTGRDFY64567.jpg", "https://i.postimg.cc/TP3z2q7Y/3-TFTGYHGUIUI.jpg", "https://i.postimg.cc/3wfPk4Vj/3-TFY56-TR7-Y5-E7.jpg", "https://i.postimg.cc/PxL0Yfng/4-ATFGU6754-E.jpg", "https://i.postimg.cc/NMWS4hg5/4-FGYH6-R75-RE.jpg", "https://i.postimg.cc/7h0ctB6X/4-FGYHR567-R.jpg", "https://i.postimg.cc/kMbk55c8/4-FTGYHDTFGY.jpg", "https://i.postimg.cc/RVx2JzsY/4-GYHUJYFU678.jpg", "https://i.postimg.cc/02GFpHhy/4-UIYGFU5-E.jpg", "https://i.postimg.cc/Z5RHC7Sg/52-GHYJUFYGU.jpg"];
-  var _gambar5 = ["https://i.postimg.cc/TY2q47pw/5644-SXDERTGF.jpg", "https://i.postimg.cc/vZQjXtdL/5-ADFTYGHTFYU657.jpg", "https://i.postimg.cc/VkDhh0fy/5-TFGUTDFU.jpg", "https://i.postimg.cc/W4zycpzd/5-TXDFGDRFY4.jpg", "https://i.postimg.cc/t4M5P3Rg/64-TDFGYHDTFYU.jpg", "https://i.postimg.cc/mr1SSPhy/67-DRFCTGRDT.jpg", "https://i.postimg.cc/W4yHNRqX/6-AFYHU6-R757.jpg", "https://i.postimg.cc/wvsb3WhH/6-DFRTY5-E7-E.jpg", "https://i.postimg.cc/7h8tzgVz/6-DRTYHDRTUDR.jpg", "https://i.postimg.cc/RCKDBDSL/6-RDETYGDRE56.jpg"];
-  var _gambar6 = ["https://i.postimg.cc/76vntq28/75-DRFTGDRFY.jpg", "https://i.postimg.cc/zBnS94CL/75-SEDXTRGFR4-ET6.jpg", "https://i.postimg.cc/28YX873R/7-DRETYGXDEST.jpg", "https://i.postimg.cc/GtF7xH75/6-F6-T7-TDF6-YE5-RD.jpg", "https://i.postimg.cc/8PbXFfrF/7-DRFT5-E7-SE5-R.jpg", "https://i.postimg.cc/MpKP1Zy9/7-DRFTYDY757.jpg", "https://i.postimg.cc/K8q9fXfx/7-DRT57-E456-T4-E.jpg", "https://i.postimg.cc/mrPm6dpM/7-YHGFJUFU.jpg", "https://i.postimg.cc/PJTzhKyS/85-TRYGDER56.jpg", "https://i.postimg.cc/x85GdHFn/86-DTGFERFTY.jpg"];
-  var _gambar7 = ["https://i.postimg.cc/5tDscwDr/8-DFRTY68-RE567.jpg", "https://i.postimg.cc/JnJPRsLz/8-FTGYHUTF7-U5.jpg", "https://i.postimg.cc/mksVkTLN/8-RTF6-YHDRFTGDES.jpg", "https://i.postimg.cc/MHm5jgXk/8-YGFUIYFUHDR.jpg", "https://i.postimg.cc/0500ZR5F/95-REDRFTG.jpg", "https://i.postimg.cc/MZ7d4f5s/9-DFRTGESDYR5-R.jpg", "https://i.postimg.cc/X7nQ0NDn/9-FRTYG5-R6.jpg", "https://i.postimg.cc/WbSSpRQg/9-FTGY6-T5757.jpg"];
-  var _x1 = Math.floor(Math.random() * _gambar1.length);
-  var _x2 = Math.floor(Math.random() * _gambar2.length);
-  var _x3 = Math.floor(Math.random() * _gambar3.length);
-  var _x4 = Math.floor(Math.random() * _gambar4.length);
-  var _x5 = Math.floor(Math.random() * _gambar5.length);
-  var _x6 = Math.floor(Math.random() * _gambar6.length);
-  var _x7 = Math.floor(Math.random() * _gambar7.length);
-  var _link1 = _gambar1[_x1];
-  var _link2 = _gambar2[_x2];
-  var _link3 = _gambar3[_x3];
-  var _link4 = _gambar4[_x4];
-  var _link5 = _gambar5[_x5];
-  var _link6 = _gambar6[_x6];
-  var _link7 = _gambar7[_x7];
-  var _hasil = [_link1, _link2, _link3, _link4, _link5, _link6, _link7];
-  var _rndx = Math.floor(Math.random() * _hasil.length);
-  var _hasil_link_gambar = _hasil[_rndx];
-  document.getElementById("result").innerHtml = _hasil_link_gambar;
+function gen(){
+var imgUrl1 = ["https://i.postimg.cc/jS4TSjRv/Screenshot-1.jpg", "https://i.postimg.cc/43PSr7Xk/Screenshot-10.jpg", "https://i.postimg.cc/NM0zYs0y/Screenshot-11.jpg", "https://i.postimg.cc/gJBB2Cwg/Screenshot-12.jpg", "https://i.postimg.cc/VvrZrKnm/Screenshot-13.jpg", "https://i.postimg.cc/Kj6qntDL/Screenshot-14.jpg", "https://i.postimg.cc/sfM34yJD/Screenshot-15.jpg", "https://i.postimg.cc/J7SMTgT3/Screenshot-16.jpg", "https://i.postimg.cc/FscmwZqM/Screenshot-17.jpg", "https://i.postimg.cc/wjpHzNY7/Screenshot-18.jpg"];
+var imgUrl2 = ["https://i.postimg.cc/TP26LXwx/Screenshot-2.jpg", "https://i.postimg.cc/x8chGfDr/Screenshot-3.jpg", "https://i.postimg.cc/J0vgPfMs/Screenshot-4.jpg", "https://i.postimg.cc/44Srf9VX/Screenshot-5.jpg", "https://i.postimg.cc/4xCqtRWM/Screenshot-6.jpg", "https://i.postimg.cc/7YJjfZn0/Screenshot-7.jpg", "https://i.postimg.cc/mgQqypqr/Screenshot-8.jpg", "https://i.postimg.cc/43s0LbkQ/Screenshot-9.jpg", "https://i.postimg.cc/Kv9rSPM5/1-AFDGVDFRHTDGF.jpg", "https://i.postimg.cc/13f09801/1-BFTGYHTFY57.jpg"];
+var imgUrl3 = ["https://i.postimg.cc/4xq6nXw7/1DRTRE4T.jpg", "https://i.postimg.cc/0NSpwhHt/2-ADRFTG5656.jpg", "https://i.postimg.cc/4dTpbwDX/2-BFGTHUTY6-RU6.jpg", "https://i.postimg.cc/0QdDtDvD/2-DFTGYHUJIYGI.jpg", "https://i.postimg.cc/90mTs0XS/2-DTGFYHRTYH.jpg", "https://i.postimg.cc/cCFYWWrJ/2-TFGYH5675-E.jpg", "https://i.postimg.cc/3r12mXnH/3-AFTGYHFJUYF5.jpg", "https://i.postimg.cc/RV0KRWDJ/3-AFTGYHT56-Y7.jpg", "https://i.postimg.cc/8PhQZCfD/3-BRFTGYHU5-RE7.jpg", "https://i.postimg.cc/W1TRCP0Z/3-GTHBTGFDY.jpg"];
+var imgUrl4 = ["https://i.postimg.cc/52gcMkmR/3-RFDTGRDFY64567.jpg", "https://i.postimg.cc/TP3z2q7Y/3-TFTGYHGUIUI.jpg", "https://i.postimg.cc/3wfPk4Vj/3-TFY56-TR7-Y5-E7.jpg", "https://i.postimg.cc/PxL0Yfng/4-ATFGU6754-E.jpg", "https://i.postimg.cc/NMWS4hg5/4-FGYH6-R75-RE.jpg", "https://i.postimg.cc/7h0ctB6X/4-FGYHR567-R.jpg", "https://i.postimg.cc/kMbk55c8/4-FTGYHDTFGY.jpg", "https://i.postimg.cc/RVx2JzsY/4-GYHUJYFU678.jpg", "https://i.postimg.cc/02GFpHhy/4-UIYGFU5-E.jpg", "https://i.postimg.cc/Z5RHC7Sg/52-GHYJUFYGU.jpg"];
+var imgUrl5 = ["https://i.postimg.cc/TY2q47pw/5644-SXDERTGF.jpg", "https://i.postimg.cc/vZQjXtdL/5-ADFTYGHTFYU657.jpg", "https://i.postimg.cc/VkDhh0fy/5-TFGUTDFU.jpg", "https://i.postimg.cc/W4zycpzd/5-TXDFGDRFY4.jpg", "https://i.postimg.cc/t4M5P3Rg/64-TDFGYHDTFYU.jpg", "https://i.postimg.cc/mr1SSPhy/67-DRFCTGRDT.jpg", "https://i.postimg.cc/W4yHNRqX/6-AFYHU6-R757.jpg", "https://i.postimg.cc/wvsb3WhH/6-DFRTY5-E7-E.jpg", "https://i.postimg.cc/7h8tzgVz/6-DRTYHDRTUDR.jpg", "https://i.postimg.cc/RCKDBDSL/6-RDETYGDRE56.jpg"];
+var imgUrl6 = ["https://i.postimg.cc/76vntq28/75-DRFTGDRFY.jpg", "https://i.postimg.cc/zBnS94CL/75-SEDXTRGFR4-ET6.jpg", "https://i.postimg.cc/28YX873R/7-DRETYGXDEST.jpg", "https://i.postimg.cc/GtF7xH75/6-F6-T7-TDF6-YE5-RD.jpg", "https://i.postimg.cc/8PbXFfrF/7-DRFT5-E7-SE5-R.jpg", "https://i.postimg.cc/MpKP1Zy9/7-DRFTYDY757.jpg", "https://i.postimg.cc/K8q9fXfx/7-DRT57-E456-T4-E.jpg", "https://i.postimg.cc/mrPm6dpM/7-YHGFJUFU.jpg", "https://i.postimg.cc/PJTzhKyS/85-TRYGDER56.jpg", "https://i.postimg.cc/x85GdHFn/86-DTGFERFTY.jpg"];
+var imgUrl7 = ["https://i.postimg.cc/5tDscwDr/8-DFRTY68-RE567.jpg", "https://i.postimg.cc/JnJPRsLz/8-FTGYHUTF7-U5.jpg", "https://i.postimg.cc/mksVkTLN/8-RTF6-YHDRFTGDES.jpg", "https://i.postimg.cc/MHm5jgXk/8-YGFUIYFUHDR.jpg", "https://i.postimg.cc/0500ZR5F/95-REDRFTG.jpg", "https://i.postimg.cc/MZ7d4f5s/9-DFRTGESDYR5-R.jpg", "https://i.postimg.cc/X7nQ0NDn/9-FRTYG5-R6.jpg", "https://i.postimg.cc/WbSSpRQg/9-FTGY6-T5757.jpg"];
+var randoma = Math.floor(Math.random() * imgUrl1.length);
+var randomb = Math.floor(Math.random() * imgUrl2.length);
+var randomc = Math.floor(Math.random() * imgUrl3.length);
+var randomd = Math.floor(Math.random() * imgUrl4.length);
+var randome = Math.floor(Math.random() * imgUrl5.length);
+var randomf = Math.floor(Math.random() * imgUrl6.length);
+var randomg = Math.floor(Math.random() * imgUrl7.length);
+var linkimga = imgUrl1[randoma];
+var linkimgb = imgUrl2[randomb];
+var linkimgc = imgUrl3[randomc];
+var linkimgd = imgUrl4[randomd];
+var linkimge = imgUrl5[randome];
+var linkimgf = imgUrl6[randomf];
+var linkimgg = imgUrl7[randomg];
+var rest = [linkimga,linkimgb,linkimgc,linkimgd,linkimge,linkimgf,linkimgg];
+var randrest = Math.floor(Math.random() * rest.length);
+var branch_key_result = rest[randrest];
+document.getElementById("result").value = branch_key_result;
 }
-
-function copy() {
-  document.getElementById("result").select(),
-    document.execCommand("copy")
+function copy(){
+document.getElementById("result").select(),
+document.execCommand("copy")
 };
